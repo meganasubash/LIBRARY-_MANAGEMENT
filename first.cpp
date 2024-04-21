112c08a407783a515ab333e148b62f7608951bea
meganasubash <162588301+meganasubash@users.noreply.github.com>
2024-04-21 20:23:44 +0530

    librarian.cpp

--- first.cpp
+++ first.cpp
@@ -414,4 +414,42 @@ void Lib::pass()
         
         get();
     }
+}//end
+void Lib::librarian()
+{
+    int i;
+    cout << "\n\t************ WELCOME LIBRARIAN ************\n";
+    cout << "\n\t\t>>Please Choose One Option:\n";
+    cout << "\n\t\t1.View BookList\n\n\t\t2.Search for a Book\n\n\t\t3.Modify/Add Book\n\n\t\t4.Issue Book\n\n\t\t5.Go to main menu\n\n\t\t6.Change Password\n\n\t\t7.Close Application\n";
+    cout << "\n\t\tEnter your choice : ";
+    cin >> i;
+    switch (i)
+    {
+    case 1:
+        booklist(2);
+        break;
+    case 2:
+        see(2);
+        break;
+    case 3:
+        modify();
+        break;
+    case 4:
+        issue();
+        break;
+    case 5:
+        system("cls");
+        get();
+        break;
+    case 6:
+        password();
+        break;
+    case 7:
+        exit(0);
+    default:
+        cout << "\n\t\tPlease enter correct option :(";
+        getch();
+        system("cls");
+        librarian();
+    }
 }
